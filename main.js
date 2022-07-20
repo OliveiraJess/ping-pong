e70fe1c09fed097488ebeae516ab1ef326bcf665
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-07-20 18:32:56 -0300

    animacao bola

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
-const folhaDesenho = document.getElementById("folha");
-const areaDesenho = folhaDesenho.getContext("2d");
+window.onload = function () {
+    setInterval(executar, 1000 / 30);
+}
 
-//medidas/tamanhos
-const larguraCampo = 600;
-const alturaCampo = 500;
-const larguraLinha = 5;
-const larguraBola = 5;
-const alturaRaquete = 50;
+//posicao bola
+let posicaoBolaY = 10;
+let posicaoBolaX = posicaoBolaY;
 
-//criando campo
-areaDesenho.fillStyle = "#286047";
-areaDesenho.fillRect(0, 0, larguraCampo, alturaCampo);
+function executar() {
+    const folhaDesenho = document.getElementById("folha");
+    const areaDesenho = folhaDesenho.getContext("2d");
 
-//linha central do campo
-areaDesenho.fillStyle = "#FDFDFD";
-areaDesenho.fillRect(larguraCampo / 2 - larguraLinha / 2, 0, larguraLinha, alturaCampo);
+    //medidas/tamanhos
+    const larguraCampo = 600;
+    const alturaCampo = 500;
+    const larguraLinha = 5;
+    const alturaRaquete = 50;
+    const diametroBola = 10;
 
-//raquete 1
-areaDesenho.fillRect(0, 30, larguraLinha, alturaRaquete);
+    //criando campo
+    areaDesenho.fillStyle = "#286047";
+    areaDesenho.fillRect(0, 0, larguraCampo, alturaCampo);
 
-//raquete 2
-areaDesenho.fillRect(larguraCampo - larguraLinha, 30, larguraLinha, alturaRaquete);
+    //linha central do campo
+    areaDesenho.fillStyle = "#FDFDFD";
+    areaDesenho.fillRect(larguraCampo / 2 - larguraLinha / 2, 0, larguraLinha, alturaCampo);
 
-//bola quadrada
-areaDesenho.fillRect(30, 30, larguraBola, larguraBola);
+    // //raquete 1
+    // areaDesenho.fillRect(0, 30, larguraLinha, alturaRaquete);
+
+    // //raquete 2
+    // areaDesenho.fillRect(larguraCampo - larguraLinha, 30, larguraLinha, alturaRaquete);
+
+    //bola quadrada
+    areaDesenho.fillRect(posicaoBolaX - (diametroBola / 2), posicaoBolaY - (diametroBola / 2), diametroBola, diametroBola);
+
+    posicaoBolaX = posicaoBolaX + 1
+    posicaoBolaY = posicaoBolaY + 1
+}
+
+
+
+
