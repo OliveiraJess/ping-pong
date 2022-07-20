3bd7bb1287d751e131c1f6d366cff8ea90a7b9de
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-07-20 17:51:03 -0300

    atrbuindo variaveis aos tamanhos

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 const folhaDesenho = document.getElementById("folha");
 const areaDesenho = folhaDesenho.getContext("2d");
 
+//medidas/tamanhos
+const larguraCampo = 600;
+const alturaCampo = 500;
+const larguraLinha = 5;
+const larguraBola = 5;
+const alturaRaquete = 50;
+
 //criando campo
 areaDesenho.fillStyle = "#286047";
-areaDesenho.fillRect(0, 0, 600, 500);
+areaDesenho.fillRect(0, 0, larguraCampo, alturaCampo);
 
 //linha central do campo
 areaDesenho.fillStyle = "#FDFDFD";
-areaDesenho.fillRect(297.5, 0, 5, 500);
+areaDesenho.fillRect(larguraCampo / 2 - larguraLinha / 2, 0, larguraLinha, alturaCampo);
 
 //raquete 1
-areaDesenho.fillRect(0, 30, 5, 50);
+areaDesenho.fillRect(0, 30, larguraLinha, alturaRaquete);
 
 //raquete 2
-areaDesenho.fillRect(595, 30, 5, 50);
+areaDesenho.fillRect(larguraCampo - larguraLinha, 30, larguraLinha, alturaRaquete);
 
 //bola quadrada
-areaDesenho.fillRect(30, 30, 5, 5);
+areaDesenho.fillRect(30, 30, larguraBola, larguraBola);
