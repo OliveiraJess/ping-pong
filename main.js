0a9f82137ed049637a97b41150fd5367d79ef040
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-07-20 17:26:34 -0300

    linha central do campo

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-var folhaDesenho = document.getElementById("folha");
-var areaDesenho = folhaDesenho.getContext("2d");
+const folhaDesenho = document.getElementById("folha");
+const areaDesenho = folhaDesenho.getContext("2d");
 
+//criando campo
 areaDesenho.fillStyle = "#286047";
 areaDesenho.fillRect(0, 0, 600, 500);
+
+//linha central do campo
+areaDesenho.fillStyle = "#FDFDFD";
+areaDesenho.fillRect(297.5, 0, 5, 500);
